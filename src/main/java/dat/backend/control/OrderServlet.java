1c814ae1b34ef7241f4286a035fa2ac22df03cef
snadering <cph-sr319@cphbusiness.dk>
2023-03-23 09:33:15 +0100

    Orderservlet & Admin orders

---
+++ src/main/java/dat/backend/control/OrderServlet.java
@@ -0,0 +1,25 @@
+package dat.backend.control;
+
+import dat.backend.model.entities.User;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "OrderServlet", value = "/OrderServlet")
+public class OrderServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user.getRole().equalsIgnoreCase("admin")){
+            request.getRequestDispatcher("WEB-INF/adminOrders.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
--- src/main/webapp/WEB-INF/admin.jsp
+++ src/main/webapp/WEB-INF/admin.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-
+<p>ADMIN$112233</p>
 </body>
 </html>
